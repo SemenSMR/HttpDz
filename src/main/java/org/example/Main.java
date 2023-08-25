5ee745569a73b3213d02e62e771bce3ebcb6b89e
Evgeniy <svorovali@mail.ru>
2023-08-25 14:46:57 +0300

    Inin File

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.entity.ContentType;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class Main {
+    public static final String URL = "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
+
+    public static ObjectMapper mapper = new ObjectMapper();
+
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)
+                        .setSocketTimeout(30000)
+                        .setRedirectsEnabled(false)
+                        .build())
+                .build();
+
+        HttpGet request = new HttpGet(URL);
+        request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
+        CloseableHttpResponse response = httpClient.execute(request);
+        InputStream content = response.getEntity().getContent();
+
+        var posts = mapper.readValue(content, new TypeReference<List<Post>>() {
+        });
+        posts.stream()
+                .filter(value -> value.getUpvotes() > 0)
+                .forEach(System.out::println);
+
+
+    }
+}
---
+++ src/main/java/org/example/Post.java
@@ -0,0 +1,56 @@
+package org.example;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Post {
+    private final String Id;
+    private final String text;
+    private final String type;
+    private final String user;
+    private final int upvotes;
+
+    public String getId() {
+        return Id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public int getUpvotes() {
+        return upvotes;
+    }
+
+    public Post(
+            @JsonProperty("id") String id,
+            @JsonProperty("text") String text,
+            @JsonProperty("type") String type,
+            @JsonProperty("user") String user,
+            @JsonProperty("upvotes") int upvotes
+    ) {
+        Id = id;
+        this.text = text;
+        this.type = type;
+        this.user = user;
+        this.upvotes = upvotes;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "Id='" + Id + '\'' +
+                ", text='" + text + '\'' +
+                ", type='" + type + '\'' +
+                ", user='" + user + '\'' +
+                ", upvotes=" + upvotes +
+                '}';
+    }
+}
